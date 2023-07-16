e2c72a460db5f337a014a7c852d039e02bff5704
wangyongxin <676425826@qq.com>
2023-07-16 23:21:08 +0800

    路由文件

---
+++ my-app/src/router.js
@@ -0,0 +1,64 @@
+import {createHashRouter} from "react-router-dom";
+import ExpLise from "./ExpList";
+import ContextBasicCase from "./useContext/basic";
+import ContextHighCase from "./useContext/high";
+import EffectBasicCase from "./useEffect/basic";
+import EffectHighCase from "./useEffect/highone";
+import EffectHighTwo from "./useEffect/hightwo";
+import ReducerBasicCase from "./useReducer/reducer_basic";
+import StateBasicCase from "./useState/Basic";
+import StateHighOne from "./useState/highone";
+import StateHighThree from "./useState/highthree";
+import StateHighTwo from "./useState/hightwo";
+import ReducerBasicCaseTwo from "./useReducer/reducer_basic2";
+
+export const router = createHashRouter([
+  {
+    path: '/',
+    element: <ExpLise/>,
+  },
+  {
+    path:'case1',
+    element: <StateBasicCase />,
+  },
+  {
+    path:'case2',
+    element: <StateHighOne/>,
+  },
+  {
+    path:'case3',
+    element: <StateHighTwo/>,
+  },
+  {
+    path:'case4',
+    element: <StateHighThree/>,
+  },
+  {
+    path:'case5',
+    element: <EffectBasicCase/>,
+  },
+  {
+    path:'case6',
+    element: <EffectHighCase/>,
+  },
+  {
+    path:'case7',
+    element: <EffectHighTwo/>,
+  },
+  {
+    path:'case8',
+    element: <ContextBasicCase />,
+  },
+  {
+    path:'case9',
+    element: <ContextHighCase />,
+  },
+  {
+    path:'case10',
+    element: <ReducerBasicCase/>,
+  },
+  {
+    path:'case11',
+    element: <ReducerBasicCaseTwo/>
+  },
+])
