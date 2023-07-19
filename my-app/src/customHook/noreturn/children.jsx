1e9525465a48f4bd841f5455ba39487093e3e974
wangyongxin <676425826@qq.com>
2023-07-19 23:48:47 +0800

    无返回值案例子组件

---
+++ my-app/src/customHook/noreturn/children.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+import { useDocTitle } from "./useDocTitle";
+
+export function NoReturnChild1() {
+  const [count, setCount] = useState(0);
+  useDocTitle(count);
+  return (
+    <div>
+      <h4>我是子组件1，我的count值为:{count}</h4>
+      <button onClick={() => {setCount(Math.floor(Math.random()*1000))}}>修改count</button>
+    </div>
+  )
+}
