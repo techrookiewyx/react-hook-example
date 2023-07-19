c4af0c30dd223b50f955c7ff91cd8be8f9ce685a
wangyongxin <676425826@qq.com>
2023-07-19 23:58:11 +0800

    自定义hook特殊返回值案例

---
+++ my-app/src/customHook/return_special/returnspl.jsx
@@ -0,0 +1,11 @@
+import { ReturnSplChild1 } from "./children";
+
+export default function ReturnSplCase() {
+  return (
+    <>
+      <h2>自定义hook带特殊返回值案例</h2>
+      <h3>有一个登录组件，功能为： 1、有一个用户名输入框 2、有一个密码输入框 3、有一个提交按钮</h3>
+      <ReturnSplChild1/>
+    </>
+  )
+}
